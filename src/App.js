f0e8b348867f9b106176a275860b15008a8e2a86
muhammadsaeid <muhammadsaeid0@gmail.com>
2019-10-11 17:38:24 +0600

    remove show user in home page

--- src/App.js
+++ src/App.js
@@ -13,15 +13,6 @@ export class App extends Component {
     alert:null
   };
 
-  // Show user
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-  //   const res = await axios.get(
-  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-  //   );
-  //   this.setState({ users: res.data, loading: false });
-  // }
-
   // Search user
   searchUsers = async text => {
     this.setState({ loading: true });
